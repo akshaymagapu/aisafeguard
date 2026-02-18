663e6984d7d79408aacf958e2d580bda1cdc89f8
Akshay <akshaymagapu@users.noreply.github.com>
2026-02-18 20:12:15 +0100

    chore(release): bump version to 0.1.1 (#11)

--- src/aisafeguard/__init__.py
+++ src/aisafeguard/__init__.py
@@ -49,7 +49,7 @@ register_output_scanner("pii", PIIOutputScanner)
 register_output_scanner("malicious_url", MaliciousURLScanner)
 register_output_scanner("relevance", RelevanceScanner)
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "Guard",
